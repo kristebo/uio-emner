22a5ea5bfd23e88270789e113c73e22957db21d7
kristebo <krister.borge@ifi.uio.no>
2015-11-04 00:37:49 +0100

    FY1120 lab1 -done

--- FYS1120/Lab1/lab1oppgave2.m
+++ FYS1120/Lab1/lab1oppgave2.m
@@ -1,8 +1,15 @@
-function []=lab1oppgave2(datapunkt)
-x=linspace(0,10,100);
-y=polyval(datapunkt, x);
-polyfit(x,y,length(x));
+function []=lab1oppgave2(min, max, datapunkt,degree)
+x = linspace(min,max,length(datapunkt));
+y=datapunkt;
+%
+p = polyfit(x, datapunkt,degree);
 
-
+x1 = linspace(min,max);
+y1 = polyval(p,x1);
+%plot
+figure
+plot(x,y,'or',x1,y1,'b')
+title('Values and fitted curve')
+legend('recorded values', 'fitted curve')
 
 end
